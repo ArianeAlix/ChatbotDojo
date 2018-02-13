646547c61ac77ef588f8e0a2970afdb5cda3897f
Ariane <alix.ariane@yahoo.fr>
2018-02-13 21:11:37 +0100

    test récupération id interlocuteur

--- routes/webhook.js
+++ routes/webhook.js
@@ -13,10 +13,14 @@ router.post('/', (req, res) => {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       var webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      var sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
     });
 
     // Returns a '200 OK' response to all requests

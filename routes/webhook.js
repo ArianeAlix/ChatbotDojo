57afdb198bb00bc26449c7b2d80cced4528f3bc3
Ariane <alix.ariane@yahoo.fr>
2018-02-13 20:58:30 +0100

    git@github.com:ArianeAlix/ChatbotDojo

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET hello world page. */
+
+// Creates the endpoint for our webhook
+app.post('/', (req, res) => {  
+
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+
 // Adds support for GET requests to our webhook
 router.get('/', (req, res) => {
 

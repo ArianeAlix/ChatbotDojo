8f1fce71a8a52d9ddad9315ae727906e5257738f
Ariane <alix.ariane@yahoo.fr>
2018-02-13 21:03:51 +0100

    j avais pas auvegardé

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 
 // Creates the endpoint for our webhook
-app.post('/', (req, res) => {  
+router.post('/', (req, res) => {
 
-  let body = req.body;
+  var body = req.body;
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
@@ -15,7 +15,7 @@ app.post('/', (req, res) => {
 
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
+      var webhook_event = entry.messaging[0];
       console.log(webhook_event);
     });
 
